d14659c6b44d3593252c845d6dcba8a01955a93a
Andre Proenca <andreproenca@hotmail.com>
2023-03-25 11:07:57 -0400

    Atualizacao do desafio004

--- desafio004.py
+++ desafio004.py
@@ -1,7 +1,9 @@
-cont = [0,1,2,3,4,5,6,7,8,9,10]
+#Faça um programa que leia um número inteiro e mostre na sua tela a sua tabuada.
 
-valor = int(input("Digite o número: "))
+cont = [1,2,3,4,5,6,7,8,9,10]
+valor = int(input("Digite um número para imprimir sua tabuada: "))
+print(f'A tabuada do número {valor} é:')
 
 for i in cont:
 
-    print(f'Resultado {valor * i}')
+    print(f'{valor} x {i} =  {valor * i}')
